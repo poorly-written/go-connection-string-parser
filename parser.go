2de0ff15e65c449d561040ee32d8da63e83a9a39
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2025-12-29 17:22:08 +0600

    basic parser structure

---
+++ parser.go
@@ -0,0 +1,77 @@
+package connection_string
+
+import "strings"
+
+type config struct{}
+
+var defaultMapping = map[string]string{
+	"user_id":  "user",
+	"user":     "user",
+	"password": "password",
+	"pass":     "password",
+}
+
+var defaultDelimiter = ';'
+
+type parser struct {
+	mapping   map[string]string
+	delimiter rune
+}
+
+func (p *parser) Delimiter(delimiter rune) *parser {
+	p.delimiter = delimiter
+
+	return p
+}
+
+func (p *parser) OverwriteMapping(mapping map[string]string) *parser {
+	p.mapping = mapping
+
+	return p
+}
+
+func (p *parser) AdditionalMapping(mapping map[string]string) *parser {
+	for k, v := range mapping {
+		p.mapping[k] = v
+	}
+
+	return p
+}
+
+func (p *parser) fromUrl(input string) (*config, error) {
+	return nil, nil
+}
+
+func (p *parser) fromDelimitedString(input string) (*config, error) {
+	return nil, nil
+}
+
+func (p *parser) Parse(input string) (*config, error) {
+	input = strings.TrimSpace(input)
+	if input == "" {
+		return &config{}, nil
+	}
+
+	if strings.Contains(input, "://") {
+		return p.fromUrl(input)
+	}
+
+	return p.fromDelimitedString(input)
+}
+
+func NewParser() *parser {
+	return &parser{
+		mapping:   defaultMapping,
+		delimiter: defaultDelimiter,
+	}
+}
+
+func Parse(input string, delimiters ...rune) (*config, error) {
+	p := NewParser()
+
+	if len(delimiters) > 0 {
+		p = p.Delimiter(delimiters[0])
+	}
+
+	return p.Parse(input)
+}
