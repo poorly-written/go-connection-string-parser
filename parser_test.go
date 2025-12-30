b71f98e121bfbe20502745c6764d8f713e8197e0
Syed Sirajul Islam Anik <sirajul.islam.anik@gmail.com>
2025-12-30 17:13:30 +0600

    tests added for get property method

--- parser_test.go
+++ parser_test.go
@@ -323,4 +323,7 @@ func TestConnectionMethods(t *testing.T) {
 	assert.Equalf(t, "example.com:5432", conn.Address(), "Address after adding port")
 
 	assert.True(t, conn.HasProperties())
+	assert.True(t, conn.GetProperty("sslmode") == "prefer", `get property should return "prefer"`)
+	assert.True(t, conn.GetProperty("schema") == "", `get property should return empty string if property is not set`)
+	assert.True(t, conn.GetProperty("schema", "public") == "public", `get property should return the default value (2nd parameter) if property is not set`)
 }
